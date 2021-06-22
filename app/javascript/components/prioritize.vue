70a4debfb73c1e6e7b66b05e93d6b47f042b4098
Yaniv Savir <yaniv@yanivsavir.com>
2021-06-22 11:16:05 -0400

    render current tasks in vue

--- app/javascript/components/prioritize.vue
+++ app/javascript/components/prioritize.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1><span>Priori</span><span>tize</span></h1>
+  <div>
+    <h1><span>Priori</span><span>tize</span></h1>
+
+    <ul>
+      <li v-for='task in tasks'>{{ task.description }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  import Task from '../models/task';
+
   export default {
-    name: 'prioritize'
+    name: 'prioritize',
+    
+    data: function () {
+      return {
+        tasks: []
+      }
+    },
+
+    methods: {
+      setTasks: function (resp) {
+        this.tasks = resp.data;
+      }
+    },
+
+    mounted: function () {
+      Task.fetch().then(this.setTasks);
+    }
   }
 </script>
