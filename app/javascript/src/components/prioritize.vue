f23abe969d03d7967b929f0669e117a8fec23956
Evan Berg <evan.m.berg@gmail.com>
2022-12-28 16:56:44 -0500

    i believe all requested tasks are complete. onto some extras

--- app/javascript/src/components/prioritize.vue
+++ app/javascript/src/components/prioritize.vue
@@ -3,17 +3,18 @@
     <h1><span class='blue'>Priori</span><span class='purple'>tize</span></h1>
 
     <h2>Tasks:</h2>
-    <button id='toggleListsButton' @click='showFullList = !showFullList'>Show All The Tasks</button>
+    <button id='toggleListsButton' @click='toggleList()'>Show All The Tasks</button>
     <ul v-if='!showFullList'>
-      <li v-for='task in tasks' v-if='!task.completed' :class="`task-${task.id}`">
+      <li v-for='task in tasks' v-if='!task.completed'>
         {{ task.description }} 
         <button class='completeBtn' @click="markComplete(task.id)">mark complete</button>
       </li>
     </ul>
     <ul v-if='showFullList'>
-      <li v-for='task in tasks' :class="`task-${task.id}`">
+      <li v-for='task in tasks' :class="task.completed ? 'completedTask' : ''">
         {{ task.description }} 
-        <button class='completeBtn' @click="markComplete(task.id)">mark complete</button>
+        <button v-if='task.completed' class='completeBtn' @click="markIncomplete(task.id)">Mark as NOT complete</button>
+        <button v-if='!task.completed' class='completeBtn' @click="markComplete(task.id)">mark complete</button>
       </li>
     </ul>
   </div>
@@ -39,6 +40,16 @@
       markComplete: function (taskId) {
         Task.updateTask(taskId, 'true');
         this.tasks.find(t=>t.id == taskId).completed = true;
+      },
+      toggleList: function () {
+        let toggleButton = document.getElementById('toggleListsButton');
+        if(this.showFullList == false){
+          this.showFullList = true
+          toggleButton.innerHTML = 'Show NonCompleted Tasks'
+        }else{
+          this.showFullList = false
+          toggleButton.innerHTML = 'Show All The Tasks'
+        }
       }
     },
 
